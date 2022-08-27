e41a2f348819bdae2196e400f50b3651f34e8136
ShiraliObul <xieraili1986@gmail.com>
2022-08-27 17:16:25 -0400

    line

--- Mapping_Lines/static/Js/logic.js
+++ Mapping_Lines/static/Js/logic.js
@@ -2,7 +2,16 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 5);
+let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+// Coordinates for each point to be used in the line.
+// let line = [
+//   [33.9416, -118.4085],
+//   [37.6213, -122.3790]
+// ];
+// // Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+//   color: "red"
+// }).addTo(map);
 //  Add a marker to the map for Los Angeles, California.
 let marker = L.marker([34.0522, -118.2437]).addTo(map);
 // L.circleMarker([34.0522, -118.2437], {
@@ -30,7 +39,7 @@ cityData.forEach(function(city) {
 
 
 // We create the tile layer that will be the background of our map. // street-v11 for day //dark-v10 fot dark
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/street-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
